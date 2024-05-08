cd5990cd5edcf50156951e5f944efc705327cdc3
JerryQ <221250194@smail.nju.edu.cn>
2024-05-08 19:22:54 +0800

    feat: add axum service

--- crates/daemon/src/main.rs
+++ crates/daemon/src/main.rs
@@ -1,8 +1,19 @@
-use daemon::State;
+use std::net::SocketAddr;
+use std::sync::Arc;
+
+use axum::Router;
+use tokio::net::TcpListener;
+
+use daemon::AppState;
 
 const CONFIG_PATH: &str = "./crates/daemon/config/config.json";
 
 #[tokio::main]
 async fn main() {
-    let state = State::new(CONFIG_PATH);
+    let state = Arc::new(AppState::new(CONFIG_PATH));
+    let listener = TcpListener::bind(SocketAddr::from(state.config.daemon))
+        .await
+        .unwrap();
+    let app = Router::new().with_state(state);
+    axum::serve(listener, app).await.unwrap()
 }
