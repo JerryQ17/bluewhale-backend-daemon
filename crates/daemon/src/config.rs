1db964b8c9e24db24cf918eec59161d6b85f5efd
JerryQ <221250194@smail.nju.edu.cn>
2024-05-10 20:31:58 +0800

    feat: canonicalize backend.working_directory

--- crates/daemon/src/config.rs
+++ crates/daemon/src/config.rs
@@ -16,9 +16,10 @@ impl Config {
     pub fn new<P: AsRef<Path>>(path: P) -> io::Result<serde_json::Result<Self>> {
         match serde_json::from_str::<Self>(&read_to_string(path)?) {
             Ok(mut config) => {
-                create_dir_all(&config.backend.working_directory)?;
-                config.backend.working_directory = canonicalize(config.backend.working_directory)?;
-                let log_dir = current_dir()?.join(&config.daemon.log_directory);
+                let bwd = current_dir()?.join(config.backend.working_directory);
+                create_dir_all(&bwd)?;
+                config.backend.working_directory = canonicalize(bwd)?;
+                let log_dir = current_dir()?.join(config.daemon.log_directory);
                 create_dir_all(&log_dir)?;
                 config.daemon.log_directory = canonicalize(log_dir)?;
                 Ok(Ok(config))
